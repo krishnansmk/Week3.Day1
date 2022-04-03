fda54d85d29c1bbef3c3fb0a8c0e329983662abd
krishnansmk <71191044+krishnansmk@users.noreply.github.com>
2022-04-03 13:09:44 +0530

    Add files via upload

---
+++ College.java
@@ -0,0 +1,18 @@
+package week3.org.college;
+
+
+public class College
+{
+	protected void collegeName()
+	{
+		System.out.println("Displayed from collegeName() inside College class");
+	}
+	protected void collegeCode()
+	{
+		System.out.println("Displayed from collegeCode() inside College class");
+	}
+	public void collegeRank()
+	{
+		System.out.println("Displayed from collegeRank() inside College class");
+	}
+}
