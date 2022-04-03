f2aecfa85e14d6adb07a3dff15a651a819a7e5e8
krishnansmk <71191044+krishnansmk@users.noreply.github.com>
2022-04-03 13:10:28 +0530

    Add files via upload

---
+++ Department.java
@@ -0,0 +1,12 @@
+package week3.org.department;
+
+import week3.org.college.College;
+
+public class Department extends College
+{
+	protected void deptName()
+	{
+		System.out.println("Displayed from deptName() inside Department class");
+	}
+	
+}
